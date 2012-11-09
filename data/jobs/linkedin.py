33972e2c6360d53609d616da147c7df1968f7641
ajmarcus <little@merman>
2012-11-08 19:22:22 -0500

    linkedin 0.2

--- data/jobs/linkedin.py
+++ data/jobs/linkedin.py
@@ -1,12 +1,12 @@
 import oauth2 as oauth
 import httplib2
-import time, os, simplejson
+import time, os, simplejson, sys
  
 # Fill the keys and secrets you retrieved after registering your app
-api_key      =   'jy0hfbp6e2bx'
-secret_key  =   'kDP7wZaJuCrUPceK'
-user_token           =   'ea4c862a-af29-4047-b351-c438d0934ebf'
-user_secret          =   'c5cc9437-265b-4886-a16f-91b99e266ebb'
+api_key = sys.argv[1]
+secret_key = sys.argv[2]
+user_token = sys.argv[3]
+user_secret = sys.argv[4]
  
 # Use your API key and secret to instantiate consumer object
 consumer = oauth.Consumer(api_key, secret_key)
@@ -18,14 +18,10 @@ access_token = oauth.Token(
  
 client = oauth.Client(consumer, access_token)
  
-# Make call to LinkedIn to retrieve your own profile
-#resp,content = client.request("http://api.linkedin.com/v1/people/~?format=json", "GET", "")
+# Make call to LinkedIn
+#response,content = client.request("http://api.linkedin.com/v1/jobs/1337:(id,customer-job-code,active,posting-date,expiration-date,posting-timestamp,company:(id,name),position:(title,location,job-functions,industries,job-type,experience-level),skills-and-experience,description-snippet,description,salary,job-poster:(id,first-name,last-name,headline),referral-bonus,site-job-url,location-description)?format=json", "GET", "")
 
-#resp,content = client.request("http://api.linkedin.com/v1/jobs/1337:(id,company,posting-date)", "GET", "")
-
-
-response,content = client.request("http://api.linkedin.com/v1/jobs/1337:(id,customer-job-code,active,posting-date,expiration-date,posting-timestamp,company:(id,name),position:(title,location,job-functions,industries,job-type,experience-level),skills-and-experience,description-snippet,description,salary,job-poster:(id,first-name,last-name,headline),referral-bonus,site-job-url,location-description)?format=json", "GET", "")
-
+response,content = client.request("http://api.linkedin.com/v1/job-search?job-title=Data+Scientist", "GET", "")
 
 print content
  
