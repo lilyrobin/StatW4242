59a2f771fd50befe321492a157a2859c93e7e7eb
ajmarcus <little@merman>
2012-11-13 22:13:34 -0500

    yay!

--- src/linkedin.r
+++ src/linkedin.r
@@ -1,16 +1,26 @@
-ds <- read.delim(file="data/jobs/linkedin_ds.tsv",
+require(ggplot2)
+require(stringr)
+require(RJSONIO)
+
+ds <- read.delim(file="data/jobs/data_scientist_title.tsv",
                  stringsAsFactors=FALSE,
                  quote="",
                  fill=FALSE)
 
-stat <- read.delim(file="data/jobs/linkedin_stat.tsv",
+stat <- read.delim(file="data/jobs/stat_keyword.tsv",
                    stringsAsFactors=FALSE,
                    quote="",
                    fill=FALSE)
 
+dataScienceRegex <- ignore.case("data scien")
+ds$is.ds <- str_detect(ds$description, dataScienceRegex)
+
+table(ds$is.ds)
+
 names(ds)
 nrow(ds)
 
+table(ds$skillsAndExperience)
 head(summary(as.factor(ds$company_name)))
 head(summary(as.factor(ds$position_title)))
 head(summary(as.factor(ds$position_location_name))/nrow(ds))
@@ -22,3 +32,39 @@ head(summary(as.factor(stat$company_name)))
 head(summary(as.factor(stat$position_title)))
 head(summary(as.factor(stat$position_location_name))/nrow(stat))
 
+jobFunctions.ds <- apply(as.array(ds$position_jobFunctions_values), 1, fromJSON)
+
+jobFunctions.ds.len <- length(jobFunctions.ds)
+jobFunctions.ds.result <- array(dim=100)
+for(i in 1:jobFunctions.ds.len) {
+  for(j in 1:length(jobFunctions.ds[[i]])) {
+    jobFunctions.ds.result[[i + j - 1 ]] <- jobFunctions.ds[[i]][[j]][["name"]]
+  }
+}
+
+jobFunctions.ds.df <- data.frame(table(jobFunctions.ds.result))
+names(jobFunctions.ds.df) <- c("Skills", "Percentage")
+jobFunctions.ds.df$Percentage <- jobFunctions.ds.df$Percentage / length(jobFunctions.ds.result)
+jobFunctions.ds.df$Career <- array(data="Data Scientist", dim=nrow(jobFunctions.ds.df))
+
+jobFunctions.stat <- apply(as.array(stat$position_jobFunctions_values), 1, fromJSON)
+
+jobFunctions.stat.len <- length(jobFunctions.stat)
+jobFunctions.stat.result <- array(dim=100)
+for(i in 1:jobFunctions.stat.len) {
+  for(j in 1:length(jobFunctions.stat[[i]])) {
+    jobFunctions.stat.result[[i + j - 1 ]] <- jobFunctions.stat[[i]][[j]][["name"]]
+  }
+}
+
+jobFunctions.stat.df <- data.frame(table(jobFunctions.stat.result))
+names(jobFunctions.stat.df) <- c("Skills", "Percentage")
+jobFunctions.stat.df$Percentage <- jobFunctions.stat.df$Percentage / length(jobFunctions.stat.result)
+jobFunctions.stat.df$Career <- array(data="Statistician", dim=nrow(jobFunctions.stat.df))
+
+jobFunctions <- rbind(jobFunctions.ds.df, jobFunctions.stat.df)
+jobFunctions.sig <- jobFunctions[which(jobFunctions$Percentage > 0.1),]
+jobFunctions.sig <- jobFunctions.sig[order(jobFunctions.sig),]
+
+ggplot(, aes(x=Skills, y=Percentage, fill=Career)) + 
+  geom_bar(stat="identity", position=position_dodge())
